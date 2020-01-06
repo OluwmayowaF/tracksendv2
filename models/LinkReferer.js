7466bda43d889244a6f75a0a4a480973bb60d00f
Helokenny <helokenny@yahoo.co.uk>
2020-01-06 16:29:16 +0100

    =sdad

--- models/LinkReferer.js
+++ models/LinkReferer.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
     },
     referer: DataTypes.STRING,
+    medium: DataTypes.STRING,
   }, {});
 
   Linkreferer.associate = function (models) {
