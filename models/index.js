16b8029100446f8451be07454e7bc7586b82c0b6
Helokenny <helokenny@yahoo.co.uk>
2020-01-31 14:11:22 +0100

    =housekeeping: refactoring media-platform numenclature; viewed message count

--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ const models = {
   Linkreferer:  sequelize.import('./LinkReferer'),
   Optout:       sequelize.import('./Optout'),
   Message:      sequelize.import('./Message'),
-  Platformtype:    sequelize.import('./Platformtype'),
+  Platformtype:    sequelize.import('./PlatformType'),
   Settingsnetwork:  sequelize.import('./SettingsNetwork'),
   Settingsuserbilling:  sequelize.import('./SettingsUserBilling'),
   Settingstopuprate:  sequelize.import('./SettingsTopupRate'),
