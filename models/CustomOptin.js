7c5cf3d180f2c1056a507e5b563818bc7fc41b3b
Helokenny <helokenny@yahoo.co.uk>
2020-03-27 16:14:34 +0100

    =after custom optin options

--- models/CustomOptin.js
+++ models/CustomOptin.js
@@ -8,8 +8,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     optin_type: DataTypes.STRING,
     optin_grps: DataTypes.STRING,
-    whatsapp_optin_msg_1: DataTypes.STRING,
-    whatsapp_optin_msg_2: DataTypes.STRING,
+    optin_msg1: DataTypes.STRING,
+    msg1_channels: DataTypes.STRING,
+    optin_msg2: DataTypes.STRING,
+    msg2_channels: DataTypes.STRING,
   }, {});
 
   Customoptin.associate = function (models) {
