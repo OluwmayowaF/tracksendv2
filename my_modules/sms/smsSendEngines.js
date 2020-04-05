a517fd1e56c1103222b6721fc31e8cf334074831
Helokenny <helokenny@yahoo.co.uk>
2020-04-05 18:15:32 +0100

    =further updates

--- my_modules/sms/smsSendEngines.js
+++ my_modules/sms/smsSendEngines.js
@@ -244,7 +244,7 @@ const smsSendEngine =  async (req, res, user_id, user_balance, sndr, info, conta
                     } else */ if (response) {
                         //   console.log(`Status code: ${response.statusCode}. Message: ${response.body}`);
                         console.log('Status: ' + response);
-                        console.log('jStatus: ' + JSON.stringify(response));
+                        // console.log('jStatus: ' + JSON.stringify(response));
                         console.log('Status code: ' + JSON.stringify(response.code));
                         // console.log('Status code: ' + response.statusCode + '; Message: ' + JSON.stringify(response.body));
                         
