fe81adda74df33e64ec054dd5e2aeb1aee8419b1
Helokenny <helokenny@yahoo.co.uk>
2020-05-03 10:54:53 +0100

    =updated env.js for prod

--- config/env.js
+++ config/env.js
@@ -1,8 +1,9 @@
 const env = {
   
   // SERVER_BASE: "https://dev2.tracksend.co",
+  // NODE_ENV: "development",
   SERVER_BASE: "https://app.tracksend.co",
-  NODE_ENV: "development",
+  NODE_ENV: "production",
 
 }
 
