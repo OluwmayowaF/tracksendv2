d16474920e9f5c3bb1a19111bc96046c3a2b2d76
Helokenny <helokenny@yahoo.co.uk>
2020-07-15 19:47:55 +0100

    =implementing zapier...

--- routes/zapier.js
+++ routes/zapier.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var zapierController = require('../controllers/ZapierController');
 var cors = require("cors");
 
-router.options ('/triggerhook/new', cors());      //  
-router.post  ('/triggerhook/new', cors(), zapierController.add);
+// router.options ('/triggerhook/new', cors());      //  
+// router.post  ('/triggerhook/new', cors(), zapierController.add);
+router.post  ('/triggerhook/new', zapierController.add);
 router.delete('/triggerhook/del', zapierController.delete);
 router.get   ('/triggerhook/testdata', zapierController.testdata);
 
